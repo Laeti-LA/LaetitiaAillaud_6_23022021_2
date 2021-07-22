86caa7fdbc51d39111142ed6eea99d82019c4718
Laetitia Aillaud <laetitia.aillaud@hotmail.fr>
2021-07-22 15:27:55 +0200

    Connexion API/cluster MongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,25 @@
+// Import Express 
 const express = require('express');
 
+// Import Mongoose
+const mongoose = require('mongoose');
+
+
 const app = express();
+
+mongoose.connect('mongodb+srv://Laeti_LA:SS63nCOINGfB3Gxi@cluster0.izpb1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie'))
+  .catch(() => console.log('Echec connexion à MongoDB'));
+
+// Headers permettant d'accéder à l'API depuis n'importe quelle origine 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
 
 app.use((req, res, next) => {
   console.log('Requête reçue !');
