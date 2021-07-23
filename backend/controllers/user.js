21a4f1ff45dcb5da07da9a19e8877937bb18b44e
Laetitia Aillaud <laetitia.aillaud@hotmail.fr>
2021-07-23 10:19:50 +0200

    Correction controllers/user

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -9,17 +9,17 @@ exports.signup = (req, res, next) => {
     // Chiffrage du mdp avec la fonction asynchrone hash
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
+            // Création nouvel utilisateur 
             const user = new User({
                 email: req.body.email,
                 password: hash
             });
-            console.log(user);
             // Ajout de l'utilisateur dans la BDD avec la fonction asynchrone save
             user.save()
                 .then(() => res.status(201).json({ message: 'Nouvel utilisateur créé' }))
                 .catch(error => res.status(400).json({ error }));
         })
-        .catch(error = res.status(500).json({error}));
+        .catch(error => res.status(500).json({ error }));
 };
 
 
